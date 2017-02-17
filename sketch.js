7b600e265aa5b7df1a67b6684d3ba9c8c626ff97
drios42 <drios42@gmail.com>
2017-02-17 12:00:50 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -24,13 +24,24 @@ var ext = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  //frameRate(15);
+  //file = new SoundFile(this, "03badnews.mp3");
 
+  // file = new SoundFile(this, "popcorn.mp3");
+  //terrain = new float [lines][bands];
   // create fft object
   fft = new p5.FFT(0.85, bands);
-  
   // create audion in
   mic = new p5.AudioIn();
-  
+
+  //in = new AudioIn(this, 0);
+
+  // start the Audio Input
+  // file.play();
+
+  // patch the AudioIn
+  // fft.input(file);
+
   //// start the Audio Input
   mic.start();
 
@@ -60,12 +71,30 @@ function draw() {
   rotateX(radians(-60));
 
   translate(0, -200);
+  //rotateZ(radians(rotZ));
+  //-translate(0, -600);
   var spectrum = fft.analyze();
+
+  // update the terrain
+  //terrain[counter] = spectrum;
+
+
 
   for (var y = 0; y < terrain.length; y++) {
     beginShape();
-
+    // for (var x = ext; x > 0; x--) {
+    //   if (y > 0) {
+    //     vertex(-x * scl, y * scl, 0);
+    //     if (y < lines-1) {
+    //       vertex(-x * scl, (y + 1) * scl, 0);
+    //     }
+    //   }
+    // }
+    //float xoff =0;
     for (var x = 0; x < spectrum.length; x++) {
+      //   terrain[x][y] = map(spectrum[x], 0, 10, -100, 8000);
+      //   terrain [x][y]= map(noise(xoff, yoff), 0, 1, -scl*5, scl*5) ;
+      //  xoff +=.1;
       if (y < lines - 1) {
         if (y > 0) {
           vertex(x * scl, y * scl, terrain[y - 1][x]);
@@ -74,24 +103,34 @@ function draw() {
           vertex(x * scl, (y + 1) * scl, terrain[y][x]);
         }
         if (x < bands - 2) {
-          vertex((x+1) * scl, (y + 1) * scl, terrain[y][x+1]);
+          vertex((x + 1) * scl, (y + 1) * scl, terrain[y][x + 1]);
         }
         if (y > 0) {
           vertex(x * scl, y * scl, terrain[y - 1][x]);
         }
-         
+
       } else {}
     }
     endShape();
+    //  yoff+=.1;
   }
 
 
+
   for (var y = lines - 1; y >= 0; y--) {
+    //for (var i = 0; i < bands; i++) {
     terrain[0] = spectrum;
     if (y > 0) {
       if (y < lines) {
         terrain[y] = terrain[y - 1];
       }
     }
+    // }
   }
+
+  //for (int i = 0; i < terrain.length; i++) {
+  //  for (int j = 0; j < bands; j++) {
+  //    terrain[i][j]=0; // reset lines at the end of scroll
+  //  }
+  //}
 }
