ef8791b736717dd7dbd73249642fb8a5a96eb74a
drios42 <drios42@gmail.com>
2017-02-17 12:03:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 //all code highly based off of Dan Shiffman's processing tutorials https://www.youtube.com/watch?v=IKB1hWWedMk
 
-//SoundFile file;
 var fft;
 var mic;
 var bands = 512; // resolution of the FFT
@@ -24,23 +23,10 @@ var ext = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  //frameRate(15);
-  //file = new SoundFile(this, "03badnews.mp3");
-
-  // file = new SoundFile(this, "popcorn.mp3");
-  //terrain = new float [lines][bands];
-  // create fft object
+ 
   fft = new p5.FFT(0.85, bands);
   // create audion in
   mic = new p5.AudioIn();
-
-  //in = new AudioIn(this, 0);
-
-  // start the Audio Input
-  // file.play();
-
-  // patch the AudioIn
-  // fft.input(file);
 
   //// start the Audio Input
   mic.start();
@@ -71,30 +57,15 @@ function draw() {
   rotateX(radians(-60));
 
   translate(0, -200);
-  //rotateZ(radians(rotZ));
-  //-translate(0, -600);
+ 
   var spectrum = fft.analyze();
 
   // update the terrain
-  //terrain[counter] = spectrum;
-
-
 
   for (var y = 0; y < terrain.length; y++) {
     beginShape();
-    // for (var x = ext; x > 0; x--) {
-    //   if (y > 0) {
-    //     vertex(-x * scl, y * scl, 0);
-    //     if (y < lines-1) {
-    //       vertex(-x * scl, (y + 1) * scl, 0);
-    //     }
-    //   }
-    // }
-    //float xoff =0;
     for (var x = 0; x < spectrum.length; x++) {
-      //   terrain[x][y] = map(spectrum[x], 0, 10, -100, 8000);
-      //   terrain [x][y]= map(noise(xoff, yoff), 0, 1, -scl*5, scl*5) ;
-      //  xoff +=.1;
+
       if (y < lines - 1) {
         if (y > 0) {
           vertex(x * scl, y * scl, terrain[y - 1][x]);
@@ -112,25 +83,16 @@ function draw() {
       } else {}
     }
     endShape();
-    //  yoff+=.1;
   }
 
 
 
   for (var y = lines - 1; y >= 0; y--) {
-    //for (var i = 0; i < bands; i++) {
     terrain[0] = spectrum;
     if (y > 0) {
       if (y < lines) {
         terrain[y] = terrain[y - 1];
       }
     }
-    // }
   }
-
-  //for (int i = 0; i < terrain.length; i++) {
-  //  for (int j = 0; j < bands; j++) {
-  //    terrain[i][j]=0; // reset lines at the end of scroll
-  //  }
-  //}
 }
